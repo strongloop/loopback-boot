09c3f8365be022d0271ec80e8fc0c555cd31161e
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2016-08-11 01:34:11 +0300

    Replace fs.existsSync calls with fs.statSync

---
+++ test/utils.test.js
@@ -0,0 +1,31 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-boot
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var boot = require('../');
+var expect = require('chai').expect;
+var sandbox = require('./helpers/sandbox');
+var appdir = require('./helpers/appdir');
+
+describe('utils', function() {
+  beforeEach(sandbox.reset);
+  beforeEach(function(done) {
+    appdir.init(done);
+  });
+  describe('fileExistsSync', function() {
+    it('returns false when a file does not exist', function() {
+      var doesNotExist = sandbox.resolve('does-not-exist.json');
+      expect(boot.utils.fileExistsSync(doesNotExist))
+        .to.equal(false);
+    });
+
+    it('returns true when a file does exist', function() {
+      var doesExist = appdir.writeConfigFileSync('does-exist.json', {
+        exists: true,
+      });
+      expect(boot.utils.fileExistsSync(doesExist))
+        .to.equal(true);
+    });
+  });
+});
